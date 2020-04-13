6581e0497f8865d8c5688cde7866458f319704ab
dbaert <dennis.baerten@nike.com>
2020-04-13 08:57:42 +0200

    Added URL encoding when retrieving a key/value for a given key or prefix

--- src/test/java/ch/carve/microprofile/config/consul/ConsulClientWrapperTest.java
+++ src/test/java/ch/carve/microprofile/config/consul/ConsulClientWrapperTest.java
@@ -10,6 +10,7 @@ import static org.mockserver.model.HttpResponse.response;
 import java.util.List;
 import java.util.Map.Entry;
 
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,8 +33,8 @@ public class ConsulClientWrapperTest {
         clientServer.when(request().withPath("/v1/status/leader")).respond(response().withBody("localhost"));
         clientServer.when(request().withPath("/v1/status/peers")).respond(response().withBody("[\"localhost:8300\"]"));
         clientServer.when(request().withPath("/v1/kv/test")).respond(response().withBody("[{\"LockIndex\":0,\"Key\":\"test\",\"Flags\":0,\"Value\":\"aGVsbG8=\",\"CreateIndex\":1,\"ModifyIndex\":2}]"));
-        clientServer.when(request().withPath("/v1/kv/testFromToken").withQueryStringParameter("token", "tokenValue"))
-                .respond(response().withBody("[{\"LockIndex\":0,\"Key\":\"testFromToken\",\"Flags\":0,\"Value\":\"aGVsbG8=\",\"CreateIndex\":1,\"ModifyIndex\":2}]"));
+        clientServer.when(request().withPath("/v1/kv/testFromToken").withQueryStringParameter("token", "tokenValue")).respond(response().withBody("[{\"LockIndex\":0,\"Key\":\"testFromToken\",\"Flags\":0,\"Value\":\"aGVsbG8=\",\"CreateIndex\":1,\"ModifyIndex\":2}]"));
+        clientServer.when(request().withPath("/v1/kv/%25dev.property.somevalue.%22com.test.app.config%22")).respond(response().withBody("[{\"LockIndex\":0,\"Key\":\"testFromToken\",\"Flags\":0,\"Value\":\"aGVsbG8=\",\"CreateIndex\":1,\"ModifyIndex\":2}]"));
         clientServer.when(request().withPath("/v1/kv/myapp")).respond(response().withBody("[{\"LockIndex\":0,\"Key\":\"test\",\"Flags\":0,\"Value\":\"aGVsbG8=\",\"CreateIndex\":1,\"ModifyIndex\":2}]"));
         clientWrapper = new ConsulClientWrapper("localhost", null, clientServer.getLocalPort(), null);
     }
@@ -69,6 +70,12 @@ public class ConsulClientWrapperTest {
     }
 
     @Test
+    public void testGetValue_keys_that_need_url_encoding() {
+        String value = clientWrapper.getValue("%dev.property.somevalue.\"com.test.app.config\"");
+        assertEquals("hello", value);
+    }
+
+    @Test
     public void testGetValue_not_found() {
         String value = clientWrapper.getValue("nope");
         assertNull(value);
@@ -83,7 +90,7 @@ public class ConsulClientWrapperTest {
     @Test
     public void testGetValue_force_reconnect() {
         clientServer.clear(request().withPath("/v1/kv/test"));
-        clientServer.when(request().withPath("/v1/kv/test")).respond(response().withStatusCode(503));
+        clientServer.when(request().withPath("/v1/kv/test")).respond(response().withStatusCode(HttpStatus.SC_SERVICE_UNAVAILABLE));
         assertThrows(OperationException.class, () -> clientWrapper.getValue("test"));
         clientServer.clear(request().withPath("/v1/kv/test"));
         clientServer.when(request().withPath("/v1/kv/test")).respond(response().withBody("[{\"LockIndex\":0,\"Key\":\"test\",\"Flags\":0,\"Value\":\"aGVsbG8=\",\"CreateIndex\":1,\"ModifyIndex\":2}]"));
